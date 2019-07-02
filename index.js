c52ff1d7d6af4f7260e47245e3f021ed0dbb3112
Guilherme Machado <guimachado007@gmail.com>
2019-07-01 22:12:54 -0300

    Design atualizado com novas cores + arquivo externo de estilo

--- index.js
+++ index.js
@@ -1,69 +1,8 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { AppRegistry, View, Text, Image, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 
 
-const Estilos = {
-    body:{
-        backgroundColor: '#0097E6',
-        flex: 1
-      },
-      header:{
-        padding: 10,
-        height: 80,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-      },
-      preview:{
-        backgroundColor: 'white',
-        height: 70
-      },
-      speakerIcon:{
-        width:60,
-        height:50
-      },
-      menu:{
-        width:65,
-        height: 50,
-        flexDirection: 'column',
-        justifyContent: 'space-between'
-      },
-      barras:{
-        backgroundColor: 'white',
-        height: 10,
-      },
-      areaPecs:{
-        height: 220,
-        backgroundColor: '#00A8FF',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      imgPecsAdicionar:{
-        width: 140,
-        height:186,
-        shadowColor: 'black',
-      },
-      teclado:{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        paddingRight: 30,
-        paddingLeft: 30,
-        justifyContent: 'space-evenly',
-        paddingTop: 10
-      },
-      scrollTeclado:{
-       flex:1
-      },
-      imgPecs:{
-        marginTop: 15,
-        height: 125,
-        width: 95
-      },
-      categorias:{
-        height: 35,
-        backgroundColor: '#DFE6E9'
-      }
-}
-const {body, header, speakerIcon, menu, barras, preview, areaPecs,imgPecsAdicionar, teclado, scrollTeclado, imgPecs, categorias} = Estilos;
+import Estilos from "./src/estilos/style.js" ;
 pressionou = () => {
     alert('pressionou né safado');
 };
@@ -91,43 +30,48 @@ export default class prototipoV1 extends Component {
                 boca: require('./imgs/pecs/partesCorpo/boca.png'),
                 mao: require('./imgs/pecs/partesCorpo/mao.png'),
                 olho: require('./imgs/pecs/partesCorpo/olho.png'),
+
+                arrayEnderecos: []
             };
         }
         alteraPec(local) {
             this.setState({enderecoImg: local});
         }
+
         render() {
             return (
-            <View style={body}>
-                 <View style={header}>
+            <View style={Estilos.body}>
+                 <View style={Estilos.header}>
                     <TouchableOpacity>
-                        <Image style={speakerIcon} source={require('./imgs/speaker-icon.png')}/>
+                        <Image style={Estilos.speakerIcon} source={require('./imgs/speaker-icon.png')}/>
                     </TouchableOpacity>
                     <TouchableOpacity>
-                        <View style={menu}>
-                        <View style={barras}></View>
-                        <View style={barras}></View>
-                        <View style={barras}></View>
+                        <View style={Estilos.menu}>
+                          <View style={Estilos.barras}></View>
+                          <View style={Estilos.barras}></View>
+                          <View style={Estilos.barras}></View>
                         </View>
                     </TouchableOpacity>
                 </View>
 
-                <View style={preview}></View>
-                    <View style={areaPecs}> 
-                        <Pecs endereco={this.state.enderecoImg} estiloPec = {imgPecsAdicionar} />
+                <View style={Estilos.preview}>
+                
+                </View>
+                    <View style={Estilos.areaPecs}> 
+                        <Pecs endereco={this.state.enderecoImg} estiloPec = {Estilos.imgPecsAdicionar} />
                     </View>
-                    <View style={categorias}></View>
+                    <View style={Estilos.categorias}></View>
                     
-                    <ScrollView style={scrollTeclado}>
-                        <View style={teclado}>
+                    <ScrollView style={Estilos.scrollTeclado}>
+                        <View style={Estilos.teclado}>
                             <TouchableOpacity onPress={ ()=>{this.alteraPec(this.state.comer)}}>
-                                <Pecs endereco={this.state.comer} estiloPec = {imgPecs}/>
+                                <Pecs endereco={this.state.comer} estiloPec = {Estilos.imgPecs}/>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={ ()=>{this.alteraPec(this.state.correr)}}>
-                                <Pecs endereco={this.state.correr} estiloPec = {imgPecs}/>
+                                <Pecs endereco={this.state.correr} estiloPec = {Estilos.imgPecs}/>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={ ()=>{this.alteraPec(this.state.beber)}}>
-                                <Pecs endereco={this.state.beber} estiloPec = {imgPecs}/>
+                                <Pecs endereco={this.state.beber} estiloPec = {Estilos.imgPecs}/>
                             </TouchableOpacity>
                         </View>
                     </ScrollView>
---
+++ src/estilos/style.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native';
+const Estilos = StyleSheet.create({
+    body:{
+        backgroundColor: 'white',
+        flex: 1
+      },
+      header:{
+        padding: 10,
+        height: 70,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: '#0097e6'
+      },
+      preview:{
+        backgroundColor: 'white',
+        borderBottomWidth: 3,
+        borderColor: '#d5d6d8',
+        height: 70
+      },
+      speakerIcon:{
+        width:55,
+        height:45
+      },
+      menu:{
+        width:55,
+        height: 40,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      },
+      barras:{
+        backgroundColor: 'white',
+        height: 8,
+        borderRadius: 10
+      },
+      areaPecs:{
+        height: 220,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      imgPecsAdicionar:{
+        width: 140,
+        height:186,
+        shadowColor: 'black',
+      },
+      teclado:{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        paddingRight: 30,
+        paddingLeft: 30,
+        justifyContent: 'space-evenly',
+        paddingTop: 10
+      },
+      scrollTeclado:{
+       flex:1
+      },
+      imgPecs:{
+        marginTop: 15,
+        height: 125,
+        width: 95
+      },
+      categorias:{
+        height: 35,
+        backgroundColor: '#dfe3e6'
+      }
+});
+export default Estilos;
