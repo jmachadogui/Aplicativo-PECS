1339ceaca751a90730092e89865e65cba1b693c7
Guilherme <guimachado007@gmail.com>
2019-08-19 10:35:27 -0300

    classe Pecs criada - Teste git

---
+++ src/Pecs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Image, TouchableOpacity,} from 'react-native';
+import Estilos from "estilos/style.js" ;
+
+export default class Pecs extends Component{
+	
+	render(){
+		return(
+			<TouchableOpacity>
+
+			</TouchableOpacity>
+		);
+	}
+}
